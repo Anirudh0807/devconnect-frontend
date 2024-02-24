54d3f100b893cb2ff6990a9c27319cea2dbf2801
Anirudh0807 <anirudhvarma9849@gmail.com>
2024-02-24 18:55:43 +0530

    Update page layout and sidebar link

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div>
       <h1 className="head-text text-left">Home</h1>
     </div>
   );

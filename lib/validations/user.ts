da309770193081cfb860c8c9045cb49fdf200f28
Arka <arkopra@gmail.com>
2024-02-25 17:24:07 +0530

    onboarding complete

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const UserValidation = z.object({
-    profile_id:z.string().min(1).url(),
+    profile_photo:z.string().min(1).url(),
     name:z.string().min(3).max(30),
     username:z.string().min(3).max(30),
     bio:z.string().min(3).max(1000),

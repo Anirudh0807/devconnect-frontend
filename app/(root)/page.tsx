6b627fec7582807155b198a8c8e8d1a7abf8109a
Arka <arkopra@gmail.com>
2024-02-26 12:47:09 +0530

    threads showcase done!

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,17 +4,15 @@ import { fetchUser } from "@/lib/actions/user.actions";
 import { UserButton, currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-
 export default async function Home() {
   const result = await fetchPosts(1, 30);
   console.log(result);
 
   const user = await currentUser();
-  if(!user) redirect('/sign-in');
+  if (!user) redirect("/sign-in");
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
-
 
   return (
     <div>
@@ -22,21 +20,22 @@ export default async function Home() {
 
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
-          <p className="no-result">No Posts found</p>
+          <p className="no-result">No threads found</p>
         ) : (
           <>
-            {result.posts.map((post) => {
+            {result.posts.map((post) => (
               <ThreadCard
-               key={post.id}
-               id={post.id}
-               currentUserId={user?.id}
-               ParentId={post.parentId}
-               author={post.author}
-               community={post.community}
-               createdAt={post.createdAt}
-               comments={post.children}
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
               />
-            })}
+            ))}
           </>
         )}
       </section>

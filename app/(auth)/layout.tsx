c036a06e66a361d4e1abb74c2d8249c6e091912b
Anirudh0807 <anirudhvarma9849@gmail.com>
2024-02-26 19:33:26 +0530

    Added the comment functionality

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,27 +1,27 @@
-import { ClerkProvider } from "@clerk/nextjs"
-import { Inter } from "next/font/google"
-import '../globals.css';
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../globals.css";
 
 export const metadata = {
-    title:'DevConnect',
-    description: 'Dev Connect is a social media platform for developers to connect and share their work and ideas.',
-}
+  title: "DevConnect",
+  description:
+    "Dev Connect is a social media platform for developers to connect and share their work and ideas.",
+};
 
-const inter = Inter({ subsets: ["latin"]})
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-     children 
-    } : {
-     children : React.ReactNode
-    }) {
-    return(
-    <ClerkProvider > 
-        <html lang = "en">
-            <body className={`${inter.className} bg-dark-1`}>
-                {children}
-            </body>
-        </html>
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">{children}</div>
+        </body>
+      </html>
     </ClerkProvider>
-    )
+  );
 }
- 
--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -32,10 +32,26 @@ const Page = async ({ params }: { params: { id: string } }) => {
 
       <div className="mt-7">
         <Comment
-         threadId = {thread._id}
-         currentUserImg = {user.imageUrl}
-         currentUserId={JSON.stringify(userInfo._id)}
+          threadId={thread._id}
+          currentUserImg={userInfo.image}
+          currentUserId={JSON.stringify(userInfo._id)}
         />
+      </div>
+
+      <div className="mt-10">
+        {thread.children.map((comment: any) => (
+          <ThreadCard
+            id={comment._id}
+            currentUserId={user.id}
+            parentId={comment.parentId}
+            content={comment.text}
+            author={comment.author}
+            community={comment.community}
+            createdAt={comment.createdAt}
+            comments={comment.children}
+            isComment
+          />
+        ))}
       </div>
     </section>
   );
